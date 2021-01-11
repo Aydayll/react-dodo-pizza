d3849d284f8647d02da2ad0f9173b6ec7b9c4356
Aydayll <makenovaay23@gmail.com>
2021-01-11 20:41:20 +0600

    create the categories

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,16 +8,6 @@ const Home = () => {
             <div class='content'>
                 <div class='container'>
                     <div class='content__top'>
-                        <div class='categories'>
-                            <ul>
-                                <li class='active'>Все</li>
-                                <li>Мясные</li>
-                                <li>Вегетарианская</li>
-                                <li>Гриль</li>
-                                <li>Острые</li>
-                                <li>Закрытые</li>
-                            </ul>
-                        </div>
                         <Sort />
                     </div>
                     <h2 class='content__title'>Все пиццы</h2>
