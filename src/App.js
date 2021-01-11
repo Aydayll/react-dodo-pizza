9b111c43b905aee9a39f91364cc82093bb0fb64a
Aydayll <makenovaay23@gmail.com>
2021-01-11 20:09:36 +0600

    add items for cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Home from './pages/home';
 import Header from './components/header';
+import {Route} from 'react-router-dom';
+import Card from './pages/cart'
+import './scss/app.scss'
 
 const App = () => {
   return (
     <div className="App">
+       <div className="wrapper">
       <Header />
-      <Home />
+      <Route exact path='/' render={Home} />
+      <Route  path='/card' render={Card} />
     </div>
+   </div>
   )
 }
 export default App;
