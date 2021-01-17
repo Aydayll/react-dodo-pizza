f053037f3ab8fd0333f16578bf5bd655f5c33880
Aydayll <makenovaay23@gmail.com>
2021-01-17 17:46:24 +0600

    add some items for home and cart

--- src/components/pizzaItem/sizes.jsx
+++ src/components/pizzaItem/sizes.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import classNames from 'classnames';
+
 const Sizes = ({ sizes }) => {
   const [activeItem, setActiveItem] = React.useState(0);
   const sizesItems = [30, 24, 50];
   const handleClickActive = (index) => setActiveItem(index);
   return (
     <ul>
-      {sizes.map((item, index) => (
+      {sizesItems.map((item, index) => (
         <li onClick={() => handleClickActive(index)}
           className={classNames({
-            active: activeItem === sizesItems[0],
-            disable: !sizes.includes(sizesItems[index]),
+            active: index === activeItem && sizes.includes(item),
+            disable: !sizes.includes(item),
 
           })}>
           {item}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,32 +1,84 @@
 import React from 'react';
 import PizzaItem from '../components/pizzaItem';
+import Item from '../components/item';
 import Sort from '../components/sort';
 import Categories from '../components/categories';
 
 const Home = () => {
-    const [pizzas, setPizzas] = React.useState([])
+  const [pizzas, setPizzas] = React.useState([]);
+  const [snacks, setSnacks] = React.useState([]);
+  const [search, setSearch] = React.useState('');
 
-    React.useEffect(() => {
-        fetch('http://localhost:3000/db.json')
-            .then(response => response.json())
-            .then(({ pizzas }) => setPizzas(pizzas))
-    },[])
+  React.useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((response) => response.json())
+      .then(({ pizzas, snacks }) => {
+        setSnacks(snacks);
+        setPizzas(pizzas);
+      });
+  }, []);
 
-    return (
-            <div className='content'>
-                <div className='container'>
-                    <div className='content__top'>
-                        <Categories items={['Мясные', 'Вегетарианская', 'Гриль', 'Острые','Закрытые' ]}/>
-                        <Sort />
-                    </div>
-                    <h2 className='content__title'>Все пиццы</h2>
-                    <div className='content__items'>
-                        {pizzas.map(item => {
-                        return <PizzaItem {...item}/>})}
-                    </div>
-                </div>
-            </div>
-    )
-}
+  const handleSearchValue = (event) => setSearch(event.target.value);
+
+  return (
+    <div className='content'>
+      <div className='container'>
+        <div className='content__top'>
+          <Categories items={['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']} />
+          <Sort />
+        </div>
+        <input
+          type='text'
+          placeholder='Введите имя для поиска'
+          onChange={(e) => {
+            handleSearchValue(e);
+          }}
+        />
+        <h2 className='content__title'>Все пиццы</h2>
+        <div className='content__items'>
+          {pizzas
+            .filter((item) => {
+              if (search === '') {
+                return item;
+              } else if (item.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())) {
+                return item;
+              }
+            })
+            .map((item) => {
+              return <PizzaItem {...item} />;
+            })}
+        </div>
+        <h2 className='content__title'>Закуски</h2>
+        <div className='content__items'>
+          {snacks
+            .filter((item) => {
+              if (search === '') {
+                return item;
+              } else if (item.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())) {
+                return item;
+              }
+            })
+            .map((item) => {
+              return <Item {...item} />;
+            })}
+        </div>
+        <h2 className='content__title'>Десерты</h2>
+        <div className='content__items'>
+          {snacks
+            .filter((item) => {
+              if (search === '') {
+                return item;
+              } else if (item.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())) {
+                return item;
+              }
+            })
+            .map((item) => {
+              return <Item {...item} />;
+            })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
